9d6c190f38c559a54bb886e11127cc5f900c7aa6
OrestisL <liaskos.orestis@gmail.com>
2024-11-24 22:34:36 +0200

    Swithc interactable

---
+++ Assets/Scripts/Interactables/SwitchInteractable.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class SwitchInteractable : BaseInteractable
+{
+    public Light Light;
+    public AnimationsController AnimationsController;
+
+    private void Start()
+    {
+        //Light = transform.parent.GetComponentInChildren<Light>(true);
+        AnimationsController = GetComponent<AnimationsController>();
+        //AnimationsController.Init();
+    }
+    public override void Interact()
+    {
+        Light.enabled = !Light.enabled;
+        AnimationsController.SetAnimatorBoolParameter("Press", Light.enabled);
+    }
+
+    public void ResetAnimParamStatus(string press) 
+    {
+        AnimationsController.SetAnimatorBoolParameter(press, false);
+    }
+}
--- Assets/Scripts/Player/Movement/AnimationsController.cs
+++ Assets/Scripts/Player/Movement/AnimationsController.cs
@@ -1,12 +1,27 @@
 using UnityEngine;
+using UnityEngine.Assertions;
 
 public class AnimationsController : MonoBehaviour
 {
     private Animator _animator;
-
+    private bool _isInit;
     private void Start()
     {
+        if (!_isInit)
+            Init();
+    }
+
+    public void Init()
+    {
+        if (_isInit)
+            return;
+
+        _isInit = true;
         _animator = GetComponentInChildren<Animator>();
+
+        if (_animator == null)
+            _animator = GetComponent<Animator>();
+
     }
 
     public void SetAnimatorFloatParameter(string name, float value)
